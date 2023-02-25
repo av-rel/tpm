57b40af784c8a54b51bb6ecceee571484db887d8
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-25 19:15:07 +0545

    test examples

--- src/main.c
+++ src/main.c
@@ -2,25 +2,19 @@
 #include "tpm.h"
 #include "../deps/fs.c"
 
-#define WIDTH 512
-#define HEIGHT 512
+#define WIDTH 900
+#define HEIGHT 600
 
 uint pixels[WIDTH * HEIGHT];
 
 int main(int argc, char** argv) {
-    char* file = "examples/images/japan.png";
+    char* file = "japan.png";
 
-    //init canvas
     Canvas canvas = tpm_canvas(pixels, WIDTH, HEIGHT);
-    //fill canvas with white color
-    tpm_fill_canvas(&canvas, 0xFFFFFFFF); //AABBGGRR
-    //draw line
-    /* tpm_draw_line(&canvas, 0, 0, canvas.height, canvas.height, 0xFF000000); */
+    canvas.pixels = tpm_fill(canvas.pixels, canvas.width, canvas.height, 0xFFFFFFFF);
+    canvas.pixels = tpm_fill_circle(canvas.pixels, canvas.width, canvas.height, canvas.width / 2, canvas.height / 2, (canvas.width + canvas.height) * 1/10, 0xFF000088);
 
-    //draw circle
-    tpm_draw_circle(&canvas, canvas.width / 2, canvas.height / 2, 50 * 4, 0xFF0000FF);
-
-    save_to_png(canvas, file);
+    fs_save_to_png(canvas, file);
 
     return 0;
 }
