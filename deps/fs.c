0394d6c4cc870704bcd7cfc3819f25a7b35fa712
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-25 12:28:07 +0545

    Save to jpg and png

--- deps/fs.c
+++ deps/fs.c
@@ -10,42 +10,22 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "./stb_image_write.h"
 
-int tpm_save_to_ppm(Canvas canvas, char* path)
+
+int save_to_png(Canvas canvas, char* path)
 {
     int rtn = 0;
-    FILE* fp;
-    if ((fp = fopen(path, "wb")) == NULL) {
-        fprintf(stderr, "ERROR: %s\n", strerror(errno));
+    if (!stbi_write_png(path, canvas.width, canvas.height, 4, canvas.pixels, sizeof(uint) * canvas.width)) {
+        fprintf(stderr, "ERROR: unable to write %s\n%s", path, strerror(errno));
         rtn = 1;
-        goto dispose;
     }
-
-    fprintf(fp, "%s\n%d %d\n%d\n", PPM_FORMAT, canvas.width, canvas.height, 255);
-
-    int c;
-    for (c = 0; c < canvas.width * canvas.height; c++){
-        uint pixel = canvas.pixels[c];
-        uint bytes[3] = {
-            (pixel >> (8 * 0))&0xFF,
-            (pixel >> (8 * 1))&0xFF,
-            (pixel >> (8 * 2))&0xFF
-        };
-        fwrite(bytes, sizeof(bytes), 1, fp);
-        if (ferror(fp)) {
-            rtn = 1;
-            goto dispose;
-        }
-    }
-
-dispose:
-    fclose(fp);
     return rtn;
 }
 
-int tpm_save_png(Canvas canvas, char* path){
+int save_to_jpg(Canvas canvas, char* path)
+{
     int rtn = 0;
-    if (!stbi_write_png(path, canvas.width, canvas.height, 4, canvas.pixels, sizeof(uint) * canvas.width)) {
-        fprintf(stderr, "ERROR: unable to write %s\n", path);
+    if (!stbi_write_jpg(path, canvas.width, canvas.height, 4, canvas.pixels, sizeof(uint) * canvas.width)) {
+        fprintf(stderr, "ERROR: unable to write %s\n%s", path, strerror(errno));
         rtn = 1;
     }
     return rtn;
