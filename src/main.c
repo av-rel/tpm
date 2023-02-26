ddf165c2ac8698809a35598e9b1d265f45f7e171
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-26 17:41:16 +0545

    draw triangle

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include "tpm.c"
-#include "tpm.h"
 #include "../deps/fs.c"
+#include "tpm.h"
+#include <stdio.h>
 
 #define WIDTH 960
-#define HEIGHT 540
+#define HEIGHT 534
 
 uint pixels[WIDTH * HEIGHT];
 
@@ -11,7 +12,11 @@ int main(int argc, char** argv) {
     char* file = "bin/test.png";
 
     TPM_Canvas canvas = tpm_canvas(pixels, WIDTH, HEIGHT);
-    tpm_fill(canvas.pixels, canvas.width, canvas.height, 0xFFFFFFFF);
-        
-    return fs_save_as_png(canvas, file);
+    tpm_fill(canvas.pixels, WIDTH, HEIGHT, 0xFFFFFFFF);
+
+    tpm_fill_triangle(canvas.pixels, WIDTH, HEIGHT, 0, 0, 100, 100, 500, 500, 0xFF000000);
+
+    fs_save_as_png(canvas, file);
+
+    return 0;
 }
