a2745cc6eccee75da03d4c7f26212106d3a863e2
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-26 20:08:17 +0545

    NAMESPACE update

--- examples/jpn_flag.c
+++ examples/jpn_flag.c
@@ -1,4 +1,4 @@
-#include "../src/tpm.c"
+#include "../src/TPM.c"
 #include "../deps/fs.c"
 
 #define WIDTH 900
@@ -9,9 +9,9 @@ uint pixels[WIDTH * HEIGHT];
 int main(int argc, char** argv) {
     char* file = "./jpn_flag.png";
     
-    TPM_Canvas canvas = tpm_canvas(pixels, WIDTH, HEIGHT);
-    canvas.pixels = tpm_fill(canvas.pixels, canvas.width, canvas.height, 0xFFFFFFFF);
-    canvas.pixels = tpm_fill_circle(canvas.pixels, canvas.width, canvas.height, canvas.width / 2, canvas.height / 2, (canvas.width + canvas.height) * 1/10, 0xFF000088);
+    TPM_Canvas canvas = TPM_init_canvas(pixels, WIDTH, HEIGHT);
+    canvas.pixels = TPM_fill(canvas.pixels, canvas.width, canvas.height, 0xFFFFFFFF);
+    canvas.pixels = TPM_fill_circle(canvas.pixels, canvas.width, canvas.height, canvas.width / 2, canvas.height / 2, (canvas.width + canvas.height) * 1/10, 0xFF000088);
 
     fs_save_as_png(canvas, file);
 
