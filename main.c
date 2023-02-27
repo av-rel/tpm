a421c295b629c324ec6bf157a666f28b8433270c
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-27 14:36:14 +0545

    ...

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include "src/tpm.c"
 #include "src/fs.c"
 
+/* TODO: Usable as CLI */
+
 #define WIDTH 512
 #define HEIGHT 512
 
 uint pixels[WIDTH * HEIGHT];
 
-int main(int argc, char** argv) {
+int main(void) {
     char* file = "bin/test.png";
 
     TPM_Canvas canvas = TPM_init_canvas(pixels, WIDTH, HEIGHT);
@@ -15,5 +17,7 @@ int main(int argc, char** argv) {
     TPM_fill_triangle(canvas.pixels, canvas.width, canvas.height, 0, 0, 100, 100, canvas.width/2, canvas.width/2, 0xFF0000FF);
 
     TPM_save_as_png(canvas.pixels, canvas.width, canvas.height, file);
+
     return 0;
 }
+
