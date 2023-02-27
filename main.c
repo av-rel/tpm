17058cf15731382be4bc8d331615ba1101fcac08
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-27 14:08:40 +0545

    Moved to root

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "src/tpm.c"
+#include "src/fs.c"
+
+#define WIDTH 512
+#define HEIGHT 512
+
+uint pixels[WIDTH * HEIGHT];
+
+int main(int argc, char** argv) {
+    char* file = "bin/test.png";
+
+    TPM_Canvas canvas = TPM_init_canvas(pixels, WIDTH, HEIGHT);
+    TPM_fill(canvas.pixels, WIDTH, HEIGHT, 0xFFFFFFFF);
+
+    TPM_fill_triangle(canvas.pixels, canvas.width, canvas.height, 0, 0, 100, 100, canvas.width/2, canvas.width/2, 0xFF0000FF);
+
+    TPM_save_as_png(canvas.pixels, canvas.width, canvas.height, file);
+    return 0;
+}
