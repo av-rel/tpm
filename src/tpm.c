7f197decea3859d2582ef9f4bc8acdfa39f80dea
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-27 13:39:41 +0545

    Header declaration merged

--- src/tpm.c
+++ src/tpm.c
@@ -1,8 +1,26 @@
-#ifndef _TPM_C_
-#define _TPM_C_
+#ifndef TPM_C_
+#define TPM_C_
 
-#include "tpm.h"
 #include "def.c"
+
+typedef unsigned int uint;
+
+typedef struct {
+	uint *pixels, width, height;
+} TPM_Canvas;
+
+TPM_Canvas TPM_init_canvas(uint *pixels, uint canvas_width, uint canvas_height);
+
+uint* TPM_fill(uint *pixels, uint width, uint height, uint color);
+uint* TPM_fill_point(uint* pixels, uint stride, int x, int y, uint color);
+uint* TPM_fill_circle(uint *pixels, uint canvas_width, uint canvas_height, int cx, int cy, uint radius, uint color);
+uint* TPM_fill_rect(uint *pixels, uint canvas_width, uint canvas_height, int x, int y, uint width, uint height, uint color);
+uint* TPM_fill_triangle(uint* pixels, uint canvas_width, uint canvas_height, int x1, int y1, int x2, int y2, int x3, int y3, uint color);
+
+uint* TPM_draw_line(uint *pixels, uint canvas_width, uint height, int x1, int y1, int x2, int y2 , uint color);
+uint* TPM_draw_circle(uint *pixels, uint canvas_width, uint canvas_height, int cx, int cy, uint radius, uint color);
+uint* TPM_draw_rect(uint *pixels, uint canvas_width, uint canvas_height, int x, int y, uint width, uint height, uint color);
+uint* TPM_draw_triangle(uint* pixels, uint canvas_width, uint canvas_height, int x1, int y1, int x2, int y2, int x3, int y3, uint color);
 
 TPM_Canvas TPM_init_canvas(uint *pixels, uint canvas_width, uint canvas_height) 
 {
