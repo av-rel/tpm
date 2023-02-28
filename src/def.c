e9adc754d8fbc5cbb20a4f606a33239898e419df
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-28 13:36:45 +0545

    _M_ prefix of math

--- src/def.c
+++ src/def.c
@@ -1,18 +1,18 @@
 #ifndef DEF_C_
 #define DEF_C_
 
-#define TPM_PI 3.14159265359
+#define TPM_M_PI 3.14159265359
 
 #define TPM_RGBA(r, g, b, a) \
     ( (((r) & 0xFF) << (8*0) ) | (((g) & 0xFF) << (8*1)) | (( (b) & 0xFF)<<(8*2)) | (((a)&0xFF)<<(8*3)) )
 
-#define TPM_ABS(n) (n < 0 ? -(n) : n)
-#define TPM_SQR(n) (n*n)
-#define TPM_MAX(a, b) (a > b ? a : b)
-#define TPM_MIN(a, b) (a < b ? a : b)
+#define TPM_M_ABS(n) (n < 0 ? -(n) : n)
+#define TPM_M_SQR(n) (n*n)
+#define TPM_M_MAX(a, b) (a > b ? a : b)
+#define TPM_M_MIN(a, b) (a < b ? a : b)
 #define TPM_SWAP(T, a, b) { T t = a; a = b; b = t; }
 
-double TPM_SQRT(double x) {
+double TPM_M_SQRT(double x) {
     double guess = x / 2.0;
     double prev_guess;
     double error = 0.000001;
