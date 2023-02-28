071831179805b5ef44e0358d33cde496ad021010
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-28 14:57:42 +0545

    RENAME: main.c => test.c

--- make.bat
+++ make.bat
@@ -1,12 +1,11 @@
 @echo off
 
 set COM=gcc
-set MAIN=main.c
-set BIN=bin
-set OUT=main.exe
+set MAIN=test.c
+set BIN=bin\test.exe
 set FLAGS=-Wall -Wextra
 
 if not exist bin mkdir bin
 
-call %COM% %MAIN% -o %BIN%\%OUT% %FLAGS%
-call %BIN%\%OUT%
+call %COM% %MAIN% -o %BIN% %FLAGS%
+call %BIN%
