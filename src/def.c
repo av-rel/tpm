081c9a8d7bd751a5d523c2e19a162bd474d01fa6
Avrel <106798140+av-rel@users.noreply.github.com>
2023-02-28 13:35:45 +0545

    TPM_RGBA

--- src/def.c
+++ src/def.c
@@ -3,6 +3,9 @@
 
 #define TPM_PI 3.14159265359
 
+#define TPM_RGBA(r, g, b, a) \
+    ( (((r) & 0xFF) << (8*0) ) | (((g) & 0xFF) << (8*1)) | (( (b) & 0xFF)<<(8*2)) | (((a)&0xFF)<<(8*3)) )
+
 #define TPM_ABS(n) (n < 0 ? -(n) : n)
 #define TPM_SQR(n) (n*n)
 #define TPM_MAX(a, b) (a > b ? a : b)
@@ -22,10 +25,14 @@ double TPM_SQRT(double x) {
     return guess;
 }
 
+#define TPM_sort_vertices(x1, y1, x2, y2) { \
+    if (y1 > y2) { TPM_SWAP(int, x1, x2); TPM_SWAP(int, y1, y2); } \
+}
+
 #define TPM_sort_triangle_points(x1, y1, x2, y2, x3, y3) { \
-    if (y1 > y2) { TPM_SWAP(int, x1, x2); TPM_SWAP(int, y1, y2); } \
-    if (y2 > y3) { TPM_SWAP(int, x2, x3); TPM_SWAP(int, y2, y3); } \
-    if (y1 > y2) { TPM_SWAP(int, x1, x2); TPM_SWAP(int, y1, y2); } \
+    TPM_sort_vertices(x1, y1, x2, y2);  \
+    TPM_sort_vertices(x2, y2, x3, y3);  \
+    TPM_sort_vertices(x1, y1, x2, y2);  \
 }
 
 #endif
