65eb0afc0440ffd22b1b040ca6200e3a1b7c2888
Avrel <106798140+av-rel@users.noreply.github.com>
2023-03-01 15:24:23 +0545

    MOVE: src/fs.c => deps/fs.c

---
+++ deps/fs.c
@@ -0,0 +1,22 @@
+#ifndef FS_C_
+#define FS_C_
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "./stb_image_write.h"
+
+int TPM_save_as_png(unsigned int *pixels, unsigned int width, unsigned int height, char* path) 
+{
+    return stbi_write_png(path, width, height, 4, pixels, sizeof(unsigned int) * width);
+}
+
+int TPM_save_as_jpg(unsigned int *pixels, unsigned int width, unsigned int height, char* path) 
+{
+    return stbi_write_jpg(path, width, height, 4, pixels, sizeof(unsigned int) * width);
+}
+
+int TPM_save_as_bmp(unsigned int *pixels, unsigned int width, unsigned int height, char* path) 
+{
+    return stbi_write_bmp(path, width, height, 4, pixels);
+}
+
+#endif
