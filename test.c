8ff55ddb45b4891c887e890bebf52550ac4a7216
Avrel <106798140+av-rel@users.noreply.github.com>
2023-03-02 19:16:20 +0545

    TEST: fill

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <dirent.h>
 
 #define TPM_SAVE_AS
 #include "./tpm.c"
@@ -13,20 +14,23 @@ uint pixels[WIDTH * HEIGHT];
 int test_fill_rect(TPM_Canvas* canvas);
 int test_draw_rect(TPM_Canvas* canvas);
 int test_draw_line(TPM_Canvas* canvas);
-int test_draw_triangle(TPM_Canvas* canvas);
 int test_draw_circle(TPM_Canvas* canvas);
 int test_fill_circle(TPM_Canvas* canvas);
-
+// int test_fill_triangle(TPM_Canvas*canvas);
+// int test_draw_triangle(TPM_Canvas* canvas);
 
 int main(int argc, char** argv) {
+    if (mkdir("bin") == -1) {}
+
     TPM_Canvas canvas = TPM_init_canvas(pixels, WIDTH, HEIGHT);
 
     if (!test_fill_rect(&canvas)) return -1;
     if (!test_draw_rect(&canvas)) return -1;
     if (!test_draw_line(&canvas)) return -1;
-    if (!test_draw_triangle(&canvas)) return -1;
     if (!test_draw_circle(&canvas)) return -1;
     if (!test_fill_circle(&canvas)) return -1;
+    // if (!test_draw_triangle(&canvas)) return -1;
+    // if (!test_fill_triangle(&canvas)) return -1;
 
     return 0;
 }
@@ -50,7 +54,7 @@ int test_draw_triangle(TPM_Canvas* canvas) {
     else printf("%s\n", "\tFAILED");
 
     return rl;
-};
+}
 
 int test_draw_line(TPM_Canvas* canvas) {
     char *file = "bin/line_draw.png";
@@ -70,7 +74,7 @@ int test_draw_line(TPM_Canvas* canvas) {
     else printf("%s\n", "\tFAILED");
 
     return rl;
-};
+}
 
 int test_fill_circle(TPM_Canvas* canvas) {
     char *file = "bin/circle_fill.png";
@@ -154,15 +158,37 @@ int test_fill_rect(TPM_Canvas* canvas)
     TPM_fill_rect(canvas, 
             0, canvas->height,
             canvas->width/5, -canvas->height/2,
-            TPM_RGBA(100, 100, 100, 255));
+            TPM_RGBA(100, 100, 100, 255)); 
+    
+    int rl = TPM_save_as_png(canvas->pixels, canvas->width, canvas->height, file);
+    printf("rectangle (fill): ");
+
+    if (rl) printf("%s\n", "ok");
+    else printf("%s\n", "\tfailed");
+
+    return rl;
+}
+
+int test_fill_triangle(TPM_Canvas* canvas){
+    char *file = "bin/triangle_fill.png";
+
+    TPM_fill(canvas, TPM_RGBA(255, 255, 255, 255));
+
+    TPM_fill_triangle(canvas,
+            0, 100,
+            canvas->width/3, canvas->height/4,
+            canvas->width/2, canvas->height/3,
+            TPM_RGBA(100, 100, 100, 255)
+            );
 
     int rl = TPM_save_as_png(canvas->pixels, canvas->width, canvas->height, file);
 
-    printf("Rectangle (fill): ");
+    printf("Triangle (fill): ");
 
     if (rl) printf("%s\n", "OK");
     else printf("%s\n", "\tFAILED");
 
     return rl;
+
 }
 
