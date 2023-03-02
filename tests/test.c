ee8cb7d3a054aa5c918e31f2ae677013f622505c
Avrel <106798140+av-rel@users.noreply.github.com>
2023-03-02 15:44:27 +0545

    TEST: render line

--- tests/test.c
+++ tests/test.c
@@ -12,15 +12,37 @@ uint pixels[WIDTH * HEIGHT];
 
 int test_fill_rect(TPM_Canvas* canvas);
 int test_draw_rect(TPM_Canvas* canvas);
+int test_draw_line(TPM_Canvas* canvas);
 
 int main(int argc, char** argv) {
     TPM_Canvas canvas = TPM_init_canvas(pixels, WIDTH, HEIGHT);
 
     if (!test_fill_rect(&canvas)) return -1;
     if (!test_draw_rect(&canvas)) return -1;
+    if (!test_draw_line(&canvas)) return -1;
 
     return 0;
 }
+
+int test_draw_line(TPM_Canvas* canvas) {
+    char *file = "tests/line_draw.png";
+
+    TPM_fill(canvas, TPM_RGBA(255, 255, 255, 255));
+
+    TPM_draw_line(canvas, 
+            canvas->width/2, canvas->height/2,
+            canvas->width, canvas->height * 4,
+            TPM_RGBA(100, 100, 100, 255));
+
+    int rl = TPM_save_as_png(canvas->pixels, canvas->width, canvas->height, file);
+
+    printf("Line (draw): ");
+
+    if (rl) printf("%s\n", "OK");
+    else printf("%s\n", "\tFAILED");
+
+    return rl;
+};
 
 int test_draw_rect(TPM_Canvas* canvas) 
 {
